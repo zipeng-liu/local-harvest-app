b67e756ccdbc736fa1350e033725ddad4fc263d1
alesstongwen <alesstongwen@gmail.com>
2024-05-01 10:33:35 -0700

    add loop for views controller

--- src/areas/vendorProduct/controllers/VendorProduct.controller.ts
+++ src/areas/vendorProduct/controllers/VendorProduct.controller.ts
@@ -4,21 +4,23 @@ import IController from "../../../interfaces/controller.interface";
 import path from "path";
 
 class vendorProductController implements IController {
-  public path = "/vendor/addItem";
+  public path = "/vendor";
   public router = express.Router();
 
   constructor() {
     this.initializeRoutes();
+    // 將靜態文件夾設置為 vendorProduct 目錄下的 styles 文件夾
+    // this.router.use(
+    //   `${path}/styles`,
+    //   express.static(path.join(__dirname, "..", "styles"))
+    // );
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}/`, this.vendorAddProduct);
+    this.router.get(`${this.path}/addItem`, this.vendorAddProduct);
   }
 
   private vendorAddProduct = (_: express.Request, res: express.Response) => {
-    const viewsPath = path.join(__dirname, "..", "views");
-    console.log(viewsPath);
-    res.app.set("views", viewsPath);
     res.render("addProduct");
   };
 }
