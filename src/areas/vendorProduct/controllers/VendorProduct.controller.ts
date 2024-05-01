0914d0eb5d5737f4931327b3a24ddae9433314c0
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-05-01 11:30:14 -0700

    add success page

--- src/areas/vendorProduct/controllers/VendorProduct.controller.ts
+++ src/areas/vendorProduct/controllers/VendorProduct.controller.ts
@@ -4,23 +4,36 @@ import IController from "../../../interfaces/controller.interface";
 import path from "path";
 
 class vendorProductController implements IController {
-  public path = "/vendor/addItem";
+  public path = "/vendor";
   public router = express.Router();
 
   constructor() {
+    // const viewsPath = path.join(__dirname, "..", "views");
+    // const staticPath = path.join(__dirname, "../../..", "public")
+
+    // this.router.use((req, res, next) => {
+    //   res.app.set("views", viewsPath);
+    //   next();
+    // });
+
+    // this.router.use(express.static(path.join(__dirname, "../../..", "public")));
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}/`, this.vendorAddProduct);
+    this.router.get(`${this.path}/addItem`, this.vendorAddProduct);
+    // Just temporally do GET method for everyone see how it looks
+    // it should be redirected after completing addProduct
+    // this.router.get(`${this.path}/success`, this.getSuccessPage) 
   }
 
   private vendorAddProduct = (_: express.Request, res: express.Response) => {
-    const viewsPath = path.join(__dirname, "..", "views");
-    console.log(viewsPath);
-    res.app.set("views", viewsPath);
     res.render("addProduct");
   };
+
+  // private getSuccessPage = (_: express.Request, res: express.Response) => {
+  //   res.render("success")
+  // }
 }
 
 export default vendorProductController;
