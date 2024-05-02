a67e9965665b6c99ae98468b6135b9d3587870a4
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-05-02 10:11:53 -0700

    complete logic for inventory page without login

--- src/areas/vendorProduct/services/IVendorProductService.ts
+++ src/areas/vendorProduct/services/IVendorProductService.ts
@@ -4,17 +4,10 @@ import IMarket from "../../../interfaces/market.interface";
 import type { Vendor, Market, Product } from "@prisma/client";
 
 export default interface IVendorProductService {
-  // addPost(message: string, username: string): Promise<void>;
+  findVendorById(vendorId: number): Promise<Vendor | null> 
 
-  // sortPosts(posts: Post[] | IPost[]): Promise<Post[] | IPost[]>;
+  addProductToVendor(vendorId: number, productData: Omit<Product, 'productId'>): Promise<Product> 
 
-  // getAllPosts(username: string): Promise<Post[] | IPost[]>;
+  findAllProductsByVendor(vendorId: number): Promise<Product[]> 
 
-  // findById(id: string): Promise<Post | IPost | undefined>;
-
-  // addCommentToPost(message: string, postId: string, userId: string): Promise<void>;
-
-  // deletePost(postId: string): Promise<void>;
-
-  // getCommentsByPostId(postId: string): Promise<Comment[] | IComment[] | undefined> 
 }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import App from "./app";
 import LandingController from "./areas/landing/controllers/Landing.controller";
 import vendorProductController from "./areas/vendorProduct/controllers/VendorProduct.controller";
+import { VendorProductService } from "./areas/vendorProduct/services/VendorProduct.service";
 
 const server = new App([
   new LandingController(),
-  new vendorProductController(),
+  new vendorProductController(new VendorProductService()),
 ]);
 
 server.start();
