e0ab3ff1d897d239836b10a443f3de9ba7d0ae69
denis <denis961111@gmail.com>
2024-05-02 02:00:27 -0700

    add vendor login logic

--- src/areas/authentication/services/Authentication.service.ts
+++ src/areas/authentication/services/Authentication.service.ts
@@ -1,59 +1,43 @@
 import DBClient from "../../../PrismaClient";
-import { IAuthenticationService } from "./IAuthentication.service";
-import ICustomer from "../../../interfaces/customer.interface";
-import IVendor from "../../../interfaces/vendor.interface";
+import { IVendorAuthenticationService } from "./IAuthentication.service";
 import type { Customer, Vendor } from "@prisma/client";
-//import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
 const SALT_ROUNDS = 10;
 
-export class VenderAuthenticationService implements IAuthenticationService {
+export class VendorAuthenticationService implements IVendorAuthenticationService {
   readonly _db: DBClient = DBClient.getInstance();
 
-  // async findUserByEmail(email: string): Promise<User | IUser | null> {
-  //   // 🚀 Talk to your real database here (I did this one for you)
-  //   return await this._db.prisma.user.findUnique({
-  //     where: {
-  //       email: email,
-  //     },
-  //   });  
-  // }
+  async findUserByEmail(email: string): Promise<Vendor | null> {
+    const vendor = await this._db.prisma.vendor.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    return vendor;
+  }
   
-  // async getUserByEmailAndPassword(email: string, password: string): Promise<User | IUser | null> {
-  //   // 🚀 Talk to your real database here
-  //   const user = await this._db.prisma.user.findUnique({
-  //     where: {
-  //       email: email,
-  //     },
-  //   });
-  //   if (user && (await bcrypt.compare(password, user.password))) {
-  //     return user;
-  //   }
-  //   return null;
-  // }
+  public async getUserByEmailAndPassword(email: string, password: string): Promise<Vendor | null> {
+    const vendor = await this._db.prisma.vendor.findUnique({
+      where: {
+        email: email,
+      },
+    });
 
-  // async createUser(user: UserDTO): Promise<User | IUser> {
-  //   // 🚀 Talk to your real database here
-  //   const hashedPassword = await bcrypt.hash(user.password, SALT_ROUNDS);
-  //   const createdUser = await this._db.prisma.user.create({
-  //     data: {
-  //       username: user.username,
-  //       email: user.email,
-  //       password: hashedPassword,
-  //       firstName: user.firstName,
-  //       lastName: user.lastName,
-  //       profilePicture: user.profilePicture, 
-  //     },
-  //   });
-  //   return createdUser;
-  // }
+    if (vendor && await bcrypt.compare(password, vendor.password)) {
+      return vendor; 
+    }
 
-  // async getUserById(id: string): Promise<User | IUser | null> {
-  //   const user = await this._db.prisma.user.findUnique({
-  //     where: {
-  //       id: id,
-  //     },
-  //   });
-  //   return user || null;
-  // }
+    return null; 
+  }
+
+  public async getUserById(id: string): Promise<Vendor | null> {
+    const user = await this._db.prisma.vendor.findUnique({
+      where: {
+        vendorId: parseInt(id, 10), 
+      },
+    });
+
+    return user || null;
+  }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import App from "./app";
 import LandingController from "./areas/landing/controllers/Landing.controller";
 import VendorAuthenticationController from "./areas/authentication/controllers/Authentication.controller";
-import { VenderAuthenticationService } from "./areas/authentication/services/Authentication.service";
+import { VendorAuthenticationService } from "./areas/authentication/services/Authentication.service";
 import VendorProductController from "./areas/vendorProduct/controllers/VendorProduct.controller";
 
 
 const server = new App([
   new LandingController(),
-  new VendorAuthenticationController(new VenderAuthenticationService()),
+  new VendorAuthenticationController(new VendorAuthenticationService()),
   new VendorProductController(),
 ]);
 
