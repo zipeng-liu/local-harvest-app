0d5aab193292cd6d50998129c565d331433eb4f1
denis <denis961111@gmail.com>
2024-05-21 21:20:12 -0700

    add change

--- src/areas/market/controllers/Market.controller.ts
+++ src/areas/market/controllers/Market.controller.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import IController from "../../../interfaces/controller.interface";
-import path from "path";
 import ensureAuthenticated from "../../../middleware/authentication.middleware";
 import { getProfileLink } from "../../../helper/profileLink";
-import { shuffle } from "../../../helper/randomFunction";
 import IMarketService from "../services/IMarket.services";
 
 class MarketController implements IController {
@@ -12,20 +10,20 @@ class MarketController implements IController {
   private _service: IMarketService;
 
   constructor(marketService: IMarketService) {
-    this.initializeRoutes();
     this._service = marketService;
+    this.initializeRoutes();
   }
 
   private initializeRoutes() {
     this.router.get(
-      `${this.path}/:id`,
-      ensureAuthenticated,
-      this.showMarketPage
-    );
-    this.router.get(
       `${this.path}/list`,
       ensureAuthenticated,
       this.showMarketList
+    );
+    this.router.get(
+      `${this.path}/:id`,
+      ensureAuthenticated,
+      this.showMarketPage
     );
   }
 
@@ -33,19 +31,23 @@ class MarketController implements IController {
     req: express.Request,
     res: express.Response
   ) => {
-    let marketId = req.params.id;
-    const market = await this._service.getMarketById(+marketId);
+    try {
+      const marketId = req.params.id;
+      const market = await this._service.getMarketById(+marketId);
+      const profileLink = getProfileLink(req, res);
 
-    const profileLink = getProfileLink(req, res);
-
-    if (profileLink) {
-      res.render("market", {
-        profileLink,
-        market,
-        marketId,
-      });
-    } else {
-      res.redirect("landing");
+      if (profileLink) {
+        res.render("market", {
+          profileLink,
+          market,
+          marketId,
+        });
+      } else {
+        res.redirect("landing");
+      }
+    } catch (error) {
+      console.error("Error fetching market page:", error);
+      res.status(500).send("Error fetching market data");
     }
   };
 
@@ -56,11 +58,11 @@ class MarketController implements IController {
     try {
       const profileLink = getProfileLink(req, res);
       const allMarkets = await this._service.getAllMarkets();
+      console.log(allMarkets);
 
       if (!profileLink) {
         res.redirect("landing");
       } else {
-        console.log("hello world");
         res.render("marketList", { profileLink, allMarkets });
       }
     } catch (error) {
