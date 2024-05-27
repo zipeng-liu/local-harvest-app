f46aaa49ea81245db0a755cf42b16f3df433e2b9
Emmy Nguyen <145530039+tastytough@users.noreply.github.com>
2024-05-27 10:37:02 -0700

    put some random results before user search on a search page

--- src/areas/search/services/ISearchService.ts
+++ src/areas/search/services/ISearchService.ts
@@ -3,7 +3,9 @@ import { Market, Vendor, Product } from "@prisma/client"
 export default interface ISearchService {
     searchResults(query: string): Promise<[Market[], Vendor[], Product[]] | undefined>;
 
-    // searchVendor(keyword: string): Promise<Vendor[]> | [];
+    getAllMarkets(): Promise<Market[]>;
 
-    // searchProduct(keyword: string): Promise<Product[]> | [];
+    getAllVendors(): Promise<Vendor[]> 
+
+    getAllProducts(): Promise<Product[]>;
 }
