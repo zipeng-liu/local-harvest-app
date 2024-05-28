3c4c719a0f32be983e40f7f1bb1de10f0fc90eed
denis <denis961111@gmail.com>
2024-05-28 00:04:13 -0700

    add one sweet alert

--- public/scripts/add-to-cart.js
+++ public/scripts/add-to-cart.js
@@ -1,32 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const addToCartForm = document.querySelectorAll(".add-to-cart-form");
+  const addToCartForm = document.querySelectorAll(".add-to-cart-form");
 
-    console.log(addToCartForm, "addToCartForm");
-    addToCartForm.forEach(form => {
-      form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const formData = new FormData(form);
+  console.log(addToCartForm, "addToCartForm");
+  addToCartForm.forEach(form => {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = new FormData(form);
 
-        const productId = "<%= product.productId %>";
-        const actionUrl = event.currentTarget.action;
+      const productId = "<%= product.productId %>";
+      const actionUrl = event.currentTarget.action;
 
-        fetch(actionUrl, {
-          method: "POST",
-          body: formData,
+      fetch(actionUrl, {
+        method: "POST",
+        body: formData,
+      })
+        .then(response => response.json())
+        .then(data => {
+          fetchCartCount();
+          if (data.success) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Success',
+              text: 'Product successfully added to cart!',
+            });
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: `Error: ${data.message}`,
+            });
+          }
         })
-          .then(response => response.json())
-          .then(data => {
-            fetchCartCount()
-            if (data.success) {
-              alert("Product successfully added to cart!");
-            } else {
-              alert(`Error: ${data.message}`);
-            }
-          })
-          .catch(error => {
-            console.error("Error submitting form:", error);
-            alert("An unexpected error occurred. Please try again later.");
+        .catch(error => {
+          console.error("Error submitting form:", error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Unexpected Error',
+            text: 'An unexpected error occurred. Please try again later.',
           });
-       });
-     });
+        });
     });
+  });
+
+  document.querySelectorAll(".secondary").forEach(image => {
+    image.addEventListener('click', function() {
+      const primaryImg = document.querySelector('.primary');
+      const tempSrc = primaryImg.src;
+
+      primaryImg.src = image.src;
+      image.src = tempSrc;
+    });
+  });
+});
